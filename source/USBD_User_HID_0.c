de3c0ed3980a34c21ae186cd58accce45dd401a0
Kudesnick <kudesnick@inbox.ru>
2020-11-01 20:05:18 +0300

    added bsp and multimedia keyboard

--- source/USBD_User_HID_0.c
+++ source/USBD_User_HID_0.c
@@ -11,6 +11,8 @@
  
 #include "rl_usb.h"
 #include "usb_hid.h"
+#include "ctl.h"
+#include "GPIO_STM32F10x.h"
  
 // User Provided HID Report Descriptor
 extern 
@@ -127,6 +129,27 @@ const uint8_t usbd_hid0_report_descriptor[] = {
             HID_EndCollection,
         HID_EndCollection,
     HID_EndCollection,
+    
+    // 37 bytes
+    0x05, 0x0C,        // Usage Page (Consumer)
+    0x09, 0x01,        // Usage (Consumer Control)
+    0xA1, 0x01,        // Collection (Application)
+    0x85, 0x03,        //   Report ID (3)
+    0x05, 0x0C,        //   Usage Page (Consumer)
+    0x15, 0x00,        //   Logical Minimum (0)
+    0x25, 0x01,        //   Logical Maximum (1)
+    0x75, 0x01,        //   Report Size (1)
+    0x95, 0x08,        //   Report Count (7)
+    0x09, 0xB5,        //   Usage (Scan Next Track)
+    0x09, 0xB6,        //   Usage (Scan Previous Track)
+    0x09, 0xB7,        //   Usage (Stop)
+    0x09, 0xB8,        //   Usage (Eject)
+    0x09, 0xCD,        //   Usage (Play/Pause)
+    0x09, 0xE2,        //   Usage (Mute)
+    0x09, 0xE9,        //   Usage (Volume Increment)
+    0x09, 0xEA,        //   Usage (Volume Decrement)
+    0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0xC0,              // End Collection
 };
 
 // Called during USBD_Initialize to initialize the USB HID class instance.
@@ -205,12 +228,11 @@ bool USBD_HID0_SetReport (uint8_t rtype, uint8_t req, uint8_t rid, const uint8_t
           if (rid == 1) // Keyboard
           {
             uint8_t leds = buf[0];
-            /*
-             * Keyboard leds
-             * 01 - Num lock 
-             * 02 - Caps lock
-             * 04 - Scroll lock
-             */
+            
+            GPIO_PinWrite(CTL_PORT, CTL_LED1_PIN, (leds & 1) ? CTL_LED_ON : CTL_LED_OFF); // Num lock
+            GPIO_PinWrite(CTL_PORT, CTL_LED2_PIN, (leds & 2) ? CTL_LED_ON : CTL_LED_OFF); // Caps lock
+            GPIO_PinWrite(CTL_PORT, CTL_LED3_PIN, (leds & 4) ? CTL_LED_ON : CTL_LED_OFF); // Scroll lock
+
           }
           break;
       }
--- source/main.c
+++ source/main.c
@@ -5,6 +5,29 @@
 #include "rl_usb.h"
 #include "GPIO_STM32F10x.h"
 #include "usb_hid_keys.h"
+#include "ctl.h"
+
+void ctl_init(void)
+{
+    GPIO_PortClock(CTL_PORT, true);
+    // Btns config
+    GPIO_PinConfigure(CTL_PORT, CTL_BTN1_PIN, GPIO_IN_PULL_DOWN, GPIO_MODE_INPUT);
+    GPIO_PinConfigure(CTL_PORT, CTL_BTN2_PIN, GPIO_IN_PULL_DOWN, GPIO_MODE_INPUT);
+    // Leds config
+    GPIO_PinConfigure(CTL_PORT, CTL_LED1_PIN, GPIO_OUT_OPENDRAIN, GPIO_MODE_OUT2MHZ);
+    GPIO_PinWrite(CTL_PORT, CTL_LED1_PIN, CTL_LED_ON);
+    GPIO_PinConfigure(CTL_PORT, CTL_LED2_PIN, GPIO_OUT_OPENDRAIN, GPIO_MODE_OUT2MHZ);
+    GPIO_PinWrite(CTL_PORT, CTL_LED2_PIN, CTL_LED_ON);
+    GPIO_PinConfigure(CTL_PORT, CTL_LED3_PIN, GPIO_OUT_OPENDRAIN, GPIO_MODE_OUT2MHZ);
+    GPIO_PinWrite(CTL_PORT, CTL_LED3_PIN, CTL_LED_ON);
+}
+
+struct btn_s
+{
+    uint8_t pin;
+    uint8_t state;
+    uint8_t next_state;
+} btn[2] = {{CTL_BTN1_PIN, CTL_BTN_OFF, CTL_BTN_OFF}, {CTL_BTN2_PIN, CTL_BTN_OFF, CTL_BTN_OFF}};
 
 /*------------------------------------------------------------------------------
  * MDK Middleware - Component ::USB:Device
@@ -23,14 +46,13 @@ void usb_handle (void)
     GPIO_PinConfigure(GPIOA, 12, GPIO_OUT_OPENDRAIN, GPIO_MODE_OUT2MHZ);
     GPIO_PinWrite(GPIOA, 12, 0);
     
-    osDelay(20);
+    osDelay(10);
     // Emulate USB disconnect (USB_DP) <-
     
     USBD_Initialize    (0);               /* USB Device 0 Initialization        */
     USBD_Connect       (0);               /* USB Device 0 Connect               */
     
     while(!USBD_Configured(0)){};
-    osDelay(2000);
 
 //    USBD_HID_GetReportTrigger(0, 2, mouse, sizeof(mouse));
 //    osDelay(100);
@@ -44,12 +66,69 @@ void usb_handle (void)
 //    USBD_HID_GetReportTrigger(0, 1, keybrd, sizeof(keybrd));
 //    osDelay(2000);                    /* 100 ms delay for sampling buttons  */
     
-    while(1){};
+    while(osDelay(10))
+    {
+        for (uint8_t i = 0; i < 2; i++)
+        {
+            uint8_t btn_tmp = GPIO_PinRead(CTL_PORT, btn[i].pin);
+            
+            if (btn[i].next_state != btn_tmp)
+            {
+                btn[i].next_state = btn_tmp;
+            }
+            else if (btn[i].state != btn[i].next_state)
+            {
+                btn[i].state = btn[i].next_state;
+                
+                switch (i)
+                {
+                    // Btn "logout"
+                    case 0:
+                        switch (btn[i].state)
+                        {
+                            case CTL_BTN_ON:
+                                {
+                                    uint8_t keybrd[] = {KEY_MOD_LMETA, 0, 0, 0 ,0, 0, 0, KEY_L};
+                                    USBD_HID_GetReportTrigger(0, 1, keybrd, sizeof(keybrd));
+                                }
+                                break;
+                            case CTL_BTN_OFF:
+                                {
+                                    uint8_t keybrd[] = {0, 0, 0, 0 ,0, 0, 0, 0};
+                                    USBD_HID_GetReportTrigger(0, 1, keybrd, sizeof(keybrd));
+                                }
+                                break;
+                        }
+                        break;
+                    // Btn "volume up"
+                    case 1:
+                        switch (btn[i].state)
+                        {
+                            case CTL_BTN_ON:
+                                {
+                                    uint8_t media[] = {1 << 6};
+                                    USBD_HID_GetReportTrigger(0, 3, media, sizeof(media));
+                                }
+                                break;
+                            case CTL_BTN_OFF:
+                                {
+                                    uint8_t media[] = {0};
+                                    USBD_HID_GetReportTrigger(0, 3, media, sizeof(media));
+                                }
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+    };
 }
 
 int main(void)
 {
     printf("main runing..\r\n");
+    
+    ctl_init();
     
     usb_handle();
     
