a1145145f11feafee206b8b5b290c4f667d47d41
Kudesnick <kudesnick@inbox.ru>
2020-11-01 13:54:51 +0300

    formatting

--- source/USBD_User_HID_0.c
+++ source/USBD_User_HID_0.c
@@ -51,112 +51,82 @@ const uint8_t usbd_hid0_report_descriptor[] = {
         HID_UsageMax(101),
         HID_Input(HID_Data | HID_Array | HID_Absolute),
     HID_EndCollection,
-/*
-    // Mouse TLC size 54 bytes
+
+    // Wheel.docx in "Enhanced Wheel Support in Windows Vista" on MS WHDC
+    // http://www.microsoft.com/whdc/device/input/wheel.mspx
+    // Mouse size 120 bytes
     HID_UsagePage(HID_USAGE_PAGE_GENERIC),
     HID_Usage(HID_USAGE_GENERIC_MOUSE),
     HID_Collection(HID_Application),
         HID_ReportID(0x02),
-        HID_Usage(HID_USAGE_GENERIC_POINTER),
-        HID_Collection(HID_Physical),
-            HID_UsagePage(HID_USAGE_PAGE_BUTTON),
-            HID_UsageMin(1),
-            HID_UsageMax(5),
-            HID_LogicalMin(0),
-            HID_LogicalMax(1),
-            HID_ReportCount(5),
-            HID_ReportSize(1),
-            HID_Input(HID_Data | HID_Variable | HID_Absolute),
-            HID_ReportCount(1),
-            HID_ReportSize(3),
-            HID_Input(HID_Constant | HID_Variable | HID_Absolute),
-            HID_UsagePage(HID_USAGE_PAGE_GENERIC),
-            HID_Usage(HID_USAGE_GENERIC_X),
-            HID_Usage(HID_USAGE_GENERIC_Y),
-            HID_Usage(HID_USAGE_GENERIC_WHEEL),
-            HID_LogicalMin((uint8_t)(-127)),
-            HID_LogicalMax(127),
-            HID_ReportSize(8),
-            HID_ReportCount(3),
-            HID_Input(HID_Data | HID_Variable | HID_Relative),
+        HID_Usage(0x02), // USAGE (Mouse)
+        HID_Collection(HID_Logical),
+            HID_Usage(HID_USAGE_GENERIC_POINTER),
+            HID_Collection(HID_Physical),
+                //-- Buttons
+                HID_UsagePage(HID_USAGE_PAGE_BUTTON),
+                HID_UsageMin(1),
+                HID_UsageMax(5),
+                HID_LogicalMin(0),
+                HID_LogicalMax(1),
+                HID_ReportCount(5),
+                HID_ReportSize(1),
+                HID_Input(HID_Data | HID_Variable | HID_Absolute),
+                //--  Padding
+                HID_ReportCount(1),
+                HID_ReportSize(3),
+                HID_Input(HID_Constant | HID_Variable | HID_Absolute),
+                //-- X,Y position
+                HID_UsagePage(HID_USAGE_PAGE_GENERIC),
+                HID_Usage(HID_USAGE_GENERIC_X),
+                HID_Usage(HID_USAGE_GENERIC_Y),
+                HID_LogicalMin((uint8_t)(-127)),
+                HID_LogicalMax(127),
+                HID_ReportSize(8),
+                HID_ReportCount(2),
+                HID_Input(HID_Data | HID_Variable | HID_Relative),
+                //-- Vertical wheel res multiplier
+                HID_Collection(HID_Logical),
+                    HID_Usage(0x48), // USAGE (Resolution Multiplier)
+                    HID_LogicalMin(0),
+                    HID_LogicalMax(1),
+                    HID_PhysicalMin(1),
+                    HID_PhysicalMax(4),
+                    HID_ReportSize(2),
+                    HID_ReportCount(1),
+                    HID_Push,
+                    HID_Feature(HID_Data | HID_Variable | HID_Absolute),
+                    //-- Vertical wheel
+                    HID_Usage(HID_USAGE_GENERIC_WHEEL),
+                    HID_LogicalMin((uint8_t)(-127)),
+                    HID_LogicalMax(127),
+                    HID_PhysicalMin(0),
+                    HID_PhysicalMax(0),
+                    HID_ReportSize(8),
+                    HID_Input(HID_Data | HID_Variable | HID_Relative),
+                HID_EndCollection,
+                //-- Horizontal wheel res multiplier
+                HID_Collection(HID_Logical),
+                                    
+                    HID_Usage(0x48), // USAGE (Resolution Multiplier)
+                    HID_Pop,
+                    HID_Feature(HID_Data | HID_Variable | HID_Absolute),
+                    //-- Padding for Feature report
+                    HID_PhysicalMin(0),
+                    HID_PhysicalMax(0),
+                    HID_ReportSize(4),
+                    HID_Feature(HID_Constant | HID_Variable | HID_Absolute),
+                    //-- Horizontal wheel
+                    HID_UsagePage(0x0c), // USAGE_PAGE (Consumer Devices)
+                    0x0a, 0x38, 0x02,  // USAGE (AC Pan)
+                    HID_LogicalMin((uint8_t)(-127)),
+                    HID_LogicalMax(127),
+                    HID_ReportSize(8),
+                    HID_Input(HID_Data | HID_Variable | HID_Relative),
+                HID_EndCollection,
+            HID_EndCollection,
         HID_EndCollection,
     HID_EndCollection,
-*/    
-//    Wheel.docx in "Enhanced Wheel Support in Windows Vista" on MS WHDC
-//    http://www.microsoft.com/whdc/device/input/wheel.mspx
-//    size 120 bytes
-    
-     0x05, 0x01,        // USAGE_PAGE (Generic Desktop)
-     0x09, 0x02,        // USAGE (Mouse)
-     0xa1, 0x01,        // COLLECTION (Application)
-     HID_ReportID(0x02),
-     0x09, 0x02,        //   USAGE (Mouse)
-     0xa1, 0x02,        //   COLLECTION (Logical)
-     0x09, 0x01,        //     USAGE (Pointer)
-     0xa1, 0x00,        //     COLLECTION (Physical)
-                        // ------------------------------  Buttons
-     0x05, 0x09,        //       USAGE_PAGE (Button)      
-     0x19, 0x01,        //       USAGE_MINIMUM (Button 1)
-     0x29, 0x05,        //       USAGE_MAXIMUM (Button 5)
-     0x15, 0x00,        //       LOGICAL_MINIMUM (0)
-     0x25, 0x01,        //       LOGICAL_MAXIMUM (1)
-     0x75, 0x01,        //       REPORT_SIZE (1)
-     0x95, 0x05,        //       REPORT_COUNT (5)
-     0x81, 0x02,        //       INPUT (Data,Var,Abs)
-                        // ------------------------------  Padding
-     0x75, 0x03,        //       REPORT_SIZE (3)
-     0x95, 0x01,        //       REPORT_COUNT (1)
-     0x81, 0x03,        //       INPUT (Cnst,Var,Abs)
-                        // ------------------------------  X,Y position
-     0x05, 0x01,        //       USAGE_PAGE (Generic Desktop)
-     0x09, 0x30,        //       USAGE (X)
-     0x09, 0x31,        //       USAGE (Y)
-     0x15, 0x81,        //       LOGICAL_MINIMUM (-127)
-     0x25, 0x7f,        //       LOGICAL_MAXIMUM (127)
-     0x75, 0x08,        //       REPORT_SIZE (8)
-     0x95, 0x02,        //       REPORT_COUNT (2)
-     0x81, 0x06,        //       INPUT (Data,Var,Rel)
-     0xa1, 0x02,        //       COLLECTION (Logical)
-                        // ------------------------------  Vertical wheel res multiplier
-     0x09, 0x48,        //         USAGE (Resolution Multiplier)
-     0x15, 0x00,        //         LOGICAL_MINIMUM (0)
-     0x25, 0x01,        //         LOGICAL_MAXIMUM (1)
-     0x35, 0x01,        //         PHYSICAL_MINIMUM (1)
-     0x45, 0x04,        //         PHYSICAL_MAXIMUM (4)
-     0x75, 0x02,        //         REPORT_SIZE (2)
-     0x95, 0x01,        //         REPORT_COUNT (1)
-     0xa4,              //         PUSH
-     0xb1, 0x02,        //         FEATURE (Data,Var,Abs)
-                        // ------------------------------  Vertical wheel
-     0x09, 0x38,        //         USAGE (Wheel)
-     0x15, 0x81,        //         LOGICAL_MINIMUM (-127)
-     0x25, 0x7f,        //         LOGICAL_MAXIMUM (127)
-     0x35, 0x00,        //         PHYSICAL_MINIMUM (0)        - reset physical
-     0x45, 0x00,        //         PHYSICAL_MAXIMUM (0)
-     0x75, 0x08,        //         REPORT_SIZE (8)
-     0x81, 0x06,        //         INPUT (Data,Var,Rel)
-     0xc0,              //       END_COLLECTION
-     0xa1, 0x02,        //       COLLECTION (Logical)
-                        // ------------------------------  Horizontal wheel res multiplier
-     0x09, 0x48,        //         USAGE (Resolution Multiplier)
-     0xb4,              //         POP
-     0xb1, 0x02,        //         FEATURE (Data,Var,Abs)
-                        // ------------------------------  Padding for Feature report
-     0x35, 0x00,        //         PHYSICAL_MINIMUM (0)        - reset physical
-     0x45, 0x00,        //         PHYSICAL_MAXIMUM (0)
-     0x75, 0x04,        //         REPORT_SIZE (4)
-     0xb1, 0x03,        //         FEATURE (Cnst,Var,Abs)
-                        // ------------------------------  Horizontal wheel
-     0x05, 0x0c,        //         USAGE_PAGE (Consumer Devices)
-     0x0a, 0x38, 0x02,  //         USAGE (AC Pan)
-     0x15, 0x81,        //         LOGICAL_MINIMUM (-127)
-     0x25, 0x7f,        //         LOGICAL_MAXIMUM (127)
-     0x75, 0x08,        //         REPORT_SIZE (8)
-     0x81, 0x06,        //         INPUT (Data,Var,Rel)
-     0xc0,              //       END_COLLECTION
-     0xc0,              //     END_COLLECTION
-     0xc0,              //   END_COLLECTION
-     0xc0               // END_COLLECTION
 };
 
 // Called during USBD_Initialize to initialize the USB HID class instance.
